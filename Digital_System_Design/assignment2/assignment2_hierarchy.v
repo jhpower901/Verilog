81333960a37793a672e213a3e4cbb9d47f9a635a
AhnBujang <jhpower901@gmail.com>
2024-04-07 22:04:36 +0900

    비동기로 완성

--- Digital_System_Design/assignment2/assignment2_hierarchy.v
+++ Digital_System_Design/assignment2/assignment2_hierarchy.v
@@ -11,24 +11,24 @@ module simpleALU (
 	always @(*) begin
 		case (opcode)
 			3'b000: begin
-				result = {{16{res_add[15]}}, res_add};
-				is_ovf = ovf_add;
+				result <= {{16{res_add[15]}}, res_add};
+				is_ovf <= ovf_add;
 			end
 			3'b001: begin
-				result = {{16{res_sub[15]}}, res_sub};
-				is_ovf = ovf_sub;
+				result <= {{16{res_sub[15]}}, res_sub};
+				is_ovf <= ovf_sub;
 			end
 			3'b010: begin
-				result = ovf_mul;
-				is_ovf = ovf_mul;
+				result <= res_mul;
+				is_ovf <= ovf_mul;
 			end
 			3'b011, 3'b100, 3'b101, 3'b110: begin
-				result = {{16{res_shi[15]}}, res_shi};
-				is_ovf = ovf_shi;
+				result <= {{16{res_shi[15]}}, res_shi};
+				is_ovf <= ovf_shi;
 			end
-			3'b111: begin
-				result = 0;
-				is_ovf = 0;
+			default: begin
+				result <= 32'bx;
+				is_ovf <= 1'bx;
 			end
 		endcase
 	end
@@ -46,7 +46,7 @@ module adder (
 );
 	assign result = in_A + in_B;
 	//overflow check;요구사항 2에서 기술한 오버플로우 조건
-	assign is_ovf = (~in_A[15] & ~in_B[15] & result[31]) | (in_A[15] & in_B[15] & ~result[31]);
+	assign is_ovf = (~in_A[15] & ~in_B[15] & result[15]) | (in_A[15] & in_B[15] & ~result[15]);
 endmodule
 
 module subtractor (
@@ -56,7 +56,7 @@ module subtractor (
 );
 	assign result = in_A - in_B;
 	//overflow check;요구사항 2에서 기술한 오버플로우 조건
-	assign is_ovf = (in_A[15] & ~in_B[15] & ~result[31]) | (~in_A[15] & in_B[15] & result[31]);
+	assign is_ovf = (in_A[15] & ~in_B[15] & ~result[15]) | (~in_A[15] & in_B[15] & result[15]);
 endmodule
 
 module multiplier (
@@ -69,22 +69,17 @@ module multiplier (
 endmodule
 
 module shifters (
-	input		signed	[15:0]	in_A, in_B,	//operand
-	input 				[1:0] 	opcode,		//operator sel
-	output	reg	signed	[15:0]	result,		//result
-	output	reg					is_ovf		//overflow checker
+	input	signed	[15:0]	in_A,
+	input			[15:0]	in_B,		//operand
+	input 			[1:0] 	opcode,		//operator sel
+	output	signed	[15:0]	result,		//result
+	output					is_ovf		//overflow checker
 );
-	always @(*) begin
-		is_ovf <= 0;
-		case(opcode)
-		//logical shift left
-			2'b11: result <= in_A << in_B;
-			//logical shift right
-			2'b00: result <= in_A >> in_B;
-			//arithmetic shift left
-			2'b01: result <= in_A <<< in_B;
-			//arithnetic shift right
-			2'b10: result <= in_A >>> in_B;
-		endcase
-	end
+	assign is_ovf = 0;
+	assign result = 	(opcode == 2'b11) ? in_A << in_B :	//logical shift left
+					(opcode == 2'b00) ? in_A >> in_B :	//logical shift right
+					(opcode == 2'b01) ? in_A <<< in_B :	//arithmetic shift left
+					(opcode == 2'b10) ? {{16{in_A[15]}}, in_A} >>> in_B :	//arithnetic shift right
+					16'bx;
+		
 endmodule
--- Digital_System_Design/assignment2/tb_assignment2.v
+++ Digital_System_Design/assignment2/tb_assignment2.v
@@ -8,7 +8,14 @@ module tb_assignmnet2 ();
 	integer				i, ERROR;
 	reg  signed	[31:0]	ref;
 
-	initial begin
+	always @(result) begin
+		if ($signed(ref) != $signed(result))
+			ERROR = ERROR + 1;
+		$display("ref: %d, res: %d\n", ref, result);
+	end
+
+	initial begin : main
+		reg [15:0] temp;
 		ERROR = 0;
 		for (i = 0; i < 10; i = i + 1) begin
 			in_A = $random;
@@ -21,50 +28,49 @@ module tb_assignmnet2 ();
 					3'b010: ref = in_A * in_B;
 					3'b011: begin
 						in_B = {1'b0, in_B[14:0]} % 17;
-						ref = in_A << in_B;
+						temp = in_A << in_B;
+						ref = {{16{temp[15]}}, temp};
 					end
 					3'b100: begin
 						in_B = {1'b0, in_B[14:0]} % 17;
-						ref = in_A >> in_B;
+						temp = in_A >> in_B;
+						ref = {{16{temp[15]}}, temp};
 					end
 					3'b101: begin
 						in_B = {1'b0, in_B[14:0]} % 17;
-						ref = in_A <<< in_B;
+						temp = in_A <<< in_B;
+						ref = {{16{temp[15]}}, temp};
 					end
 					3'b110: begin
 						in_B = {1'b0, in_B[14:0]} % 17;
-						ref = in_A >>> in_B;
+						temp = in_A >>> in_B;
+						ref = {{16{temp[15]}}, temp};
 					end
-					default: ref = 0;
+					default: ref = 32'bx;
 				endcase
-
-				if ($signed(ref) != $signed(result)) begin
-					ERROR = ERROR + 1;
-				end
-				$display("ref: %d, res: %d\n", ref, result);
 				#100;
 			end
 		end
 		$display("The number of errors: %d\n", ERROR);
 
-		//overflow 1
+		//overflow 1 p + p = n
 		in_A = {4'b0111, 12'hFFF};
 		in_B = 16'b1;
 		opcode = 3'b000;
 		#100;
-		//overflow 2
-		in_A = 16'hFFFF;
-		in_B = {1'b1, 15'b1};
+		//overflow 2 n + n = p
+		in_A = {1'b1, 15'b0};
+		in_B = {1'b1, 15'b0};
 		opcode = 3'b000;
 		#100;
-		//overflow 3
-		in_A = 16'hFFFF;
-		in_B = {16'b1};
-		opcode = 3'b001;
-		#100;
-		//overflow 4
+		//overflow 3 p - n = n
 		in_A = 16'b1;
 		in_B = {1'b1, 15'b0};
+		opcode = 3'b001;
+		#100;
+		//overflow 4 n - p = p
+		in_A = {1'b1, 15'b0};
+		in_B = 16'b1;
 		opcode = 3'b001;
 		#100;
 
