aea72ec4c6efd6ad8c3d7bf26cf1d316792628ca
Ahnbujang <jhpower901@gmail.com>
2024-05-16 05:50:10 +0900

    DSD assignment3 완료

--- Digital_System_Design/assignment3/assignment3.v
+++ Digital_System_Design/assignment3/assignment3.v
@@ -15,7 +15,7 @@
 module hunch_fsm(
     input CLK, RST,
     input A, B, C,
-    output reg  [2:0]   WINNER_DISP
+    output reg  [2:0]   WINNER_DISP,
     output reg          A_DISP, B_DISP, C_DISP
 );
 // state 정의
@@ -35,15 +35,88 @@ localparam S_DRAW   = 10;
 reg [3:0]   c_state;    //current state
 reg [3:0]   n_state;    //next state
 
+always @(posedge CLK or posedge RST) begin
+    if (RST) begin
+        A_DISP <= 0;
+        B_DISP <= 0;
+        C_DISP <= 0;
+    end else begin
+        A_DISP <= A_DISP|A;
+        B_DISP <= B_DISP|B;
+        C_DISP <= C_DISP|C;
+    end
+end
 
 
-// part1: register  sequential logic
+// part1: Initialize and update the state register
 always @(posedge CLK or posedge RST) begin
     if (RST)    n_state <= S_INIT;
     else        c_state <= n_state;
 end
 
-//part2: Next-State Logic
+//part2: Determine next state
+always @(*) begin
+    case (c_state)
+        S_INIT:
+            begin
+                case ({C, B, A})
+                    3'b000: n_state = c_state;
+                    3'b001: n_state = S_AS;
+                    3'b010: n_state = S_BS;
+                    3'b100: n_state = S_CS;
+                    3'b011: n_state = S_CW;
+                    3'b101: n_state = S_BW;
+                    3'b110: n_state = S_AW;
+                    3'b111: n_state = S_DRAW;
+                    default: n_state = c_state;
+                endcase
+            end
+        S_AS:
+            begin
+                case ({C, B})
+                    3'b00: n_state = c_state;
+                    3'b01: n_state = S_ABW;
+                    3'b10: n_state = S_ACW;
+                    3'b11: n_state = S_AW;
+                    default: n_state = c_state;
+                endcase
+            end
+        S_BS:
+            begin
+                case ({C, A})
+                    3'b00: n_state = c_state;
+                    3'b01: n_state = S_ABW;
+                    3'b10: n_state = S_BCW;
+                    3'b11: n_state = S_BW;
+                    default: n_state = c_state;
+                endcase
+            end
+        S_CS:
+            begin
+                case ({B, A})
+                    3'b00: n_state = c_state;
+                    3'b01: n_state = S_ACW;
+                    3'b10: n_state = S_BCW;
+                    3'b11: n_state = S_CW;
+                    default: n_state = c_state;
+                endcase
+            end
+        default: n_state = c_state;
+    endcase
+end
 
+//part3 : Determine output
+always @(*) begin
+    case (c_state)
+        S_AW:   WINNER_DISP = 3'b001;
+        S_BW:   WINNER_DISP = 3'b010;
+        S_CW:   WINNER_DISP = 3'b100;
+        S_ABW:  WINNER_DISP = 3'b011;
+        S_BCW:  WINNER_DISP = 3'b110;
+        S_ACW:  WINNER_DISP = 3'b101;
+        S_DRAW: WINNER_DISP = 3'b111;
+        default: WINNER_DISP = 3'b000;
+    endcase
+end
 
 endmodule
--- Digital_System_Design/assignment3/tb_assignment3.v
+++ Digital_System_Design/assignment3/tb_assignment3.v
@@ -1,15 +1,102 @@
+`timescale 1ns/1ns
 module tb_assignment3;
-wire CLK;               //clock
-wire RST;               //active high reset signal
-wire [2:0] ABC_input;   //ABC state input
-reg  [2:0] WINNER_DISP; //winner display {C, B, A}
-reg  A_DISP, B_DISP, C_DISP //A, B, C state indicater
+reg CLK;               //clock
+reg RST;               //active high reset signal
+reg [2:0] ABC_input;   //ABC state input {C, B, A}
+wire  [2:0] WINNER_DISP; //winner display {C, B, A}
+wire  A_DISP, B_DISP, C_DISP; //A, B, C state indicater
 
 //generate clock (DSD_5_SeqLogicIntro, p.22)
-inital CLK = 0;
+initial begin
+    CLK = 1'b1;
+    ABC_input = 3'b000;
+    $monitor ($time, "{%b, %b, %b} -> %b(%b%b%b)", 
+                ABC_input[2], ABC_input[1], ABC_input[0],
+                WINNER_DISP, C_DISP, B_DISP, A_DISP);
+end
 always #5 CLK = ~CLK;
 
+//test case
+initial begin
+    RST = 1'b1; #15; RST = 1'b0; #5;
 
+    //case study 1
+    ABC_input = 3'b000; #10;
+    ABC_input = 3'b001; #10;
+    ABC_input = 3'b100; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
 
+    //case study 2
+    ABC_input = 3'b001; #10;
+    ABC_input = 3'b110; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
 
+    //case study 3
+    ABC_input = 3'b000; #10;
+    ABC_input = 3'b111; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-1 A-B
+    ABC_input = 3'b001; #10;
+    ABC_input = 3'b010; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-1 A-C
+    ABC_input = 3'b001; #10;
+    ABC_input = 3'b100; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-1 B-A
+    ABC_input = 3'b010; #10;
+    ABC_input = 3'b001; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-1 B-C
+    ABC_input = 3'b010; #10;
+    ABC_input = 3'b100; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-1 C-A
+    ABC_input = 3'b100; #10;
+    ABC_input = 3'b001; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-1 C-B
+    ABC_input = 3'b100; #10;
+    ABC_input = 3'b010; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-2 A
+    ABC_input = 3'b001; #10;
+    ABC_input = 3'b110; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-2 B
+    ABC_input = 3'b010; #10;
+    ABC_input = 3'b101; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 1-2 C
+    ABC_input = 3'b100; #10;
+    ABC_input = 3'b011; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 2-1 A
+    ABC_input = 3'b110; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 2-1 B
+    ABC_input = 3'b101; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+
+    //case: 2-1 C
+    ABC_input = 3'b011; #10;
+    #10; RST = 1'b1; #15; RST = 1'b0; #5;
+    
+
+    $stop;
+end
+
+hunch_fsm DUT (.CLK(CLK), .RST(RST), .A(ABC_input[0]), .B(ABC_input[1]), .C(ABC_input[2]),
+               .WINNER_DISP(WINNER_DISP), .A_DISP(A_DISP), .B_DISP(B_DISP), .C_DISP(C_DISP));
 endmodule
